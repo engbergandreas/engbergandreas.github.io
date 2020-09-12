64eba1cadcaf2e0ee916b77f54fbd22594fc0c82
Andreas <adde.engberg@gmail.com>
2020-09-12 22:09:17 +0200

    Updates

---
+++ precache-manifest.0f1ad5f947de37bda5cd416df5a67e91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950b97ad59a2a6f46bffdb8424a35260",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89470ea22c088244d424",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d0a923a2c9a421251ab5",
+    "url": "/static/js/2.0056059c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0056059c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89470ea22c088244d424",
+    "url": "/static/js/main.945a7d7c.chunk.js"
+  },
+  {
+    "revision": "25f00cf8aa83b269d5e8",
+    "url": "/static/js/runtime-main.2ff669e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
