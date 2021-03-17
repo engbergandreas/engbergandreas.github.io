9fbb3e93811d564d7605a82feae88300cb531851
Andreas <adde.engberg@gmail.com>
2021-03-17 21:41:40 +0100

    Update header.js

--- src/pages/header.js
+++ src/pages/header.js
@@ -6,14 +6,14 @@ function Header() {
 
 
     return (
-        <div>
+        <header>
             <nav className="wrapper">
                 <NavLink exact activeClassName="activePage" className="wrapperChild" to="/">Andreas Engberg</NavLink >
                 <NavLink activeClassName="activePage" className="wrapperChild" to="/portfolio">Portfolio</NavLink>
                 <NavLink activeClassName="activePage" className="wrapperChild" to="/projects">Projects</NavLink>
                 <NavLink activeClassName="activePage" className="wrapperChild" to="/about">About</NavLink>
             </nav>
-        </div>
+        </header>
     );
 }
 
