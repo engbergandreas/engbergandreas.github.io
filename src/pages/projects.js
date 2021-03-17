b43c2fcae035b1d32518dcb14d7bc89fc59aaea9
Andreas <adde.engberg@gmail.com>
2021-03-17 21:42:27 +0100

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -16,25 +16,36 @@ const CATEGORIES = ['SHOW ALL', 'GAMES', 'COMPUTER GRAPHICS', 'PROGRAMMING', 'WE
 
 function Projects({ selectedCat, onChangeFunction, projects }) {
     //const [projectsToShow, setProjectsToShow] = useState(PROJECTS);
-
+    /*
     function filterProjectSelection(category) {
         onChangeFunction(category);
         //const filteredProjects = PROJECTS.filter(project => project.categories.includes(category.toLowerCase()));
         //category === 'SHOW ALL' ? setProjectsToShow(PROJECTS) : setProjectsToShow(filteredProjects);
     }
+    */
     function DisplayCategories() {
         return (
             <div className={'flexWrapper'} style={{ display: 'flex', justifyContent: 'space-evenly' }}>
                 {
-                    CATEGORIES.map((i, index) => (
-                        <button key={index} className={i === selectedCat ? 'activeCategory category' : 'category'}
-                            onClick={() => onChangeFunction(i)}
+                    CATEGORIES.map((category, index) => (
+                        <button key={index} className={category === selectedCat ? 'activeCategory category' : 'category'}
+                            onClick={() => onChangeFunction(category)}
                         >
-                            {i}
+                            {category}
                         </button >
                     ))
                 }
             </div >
+        );
+    }
+    function ProjectThumbnail({ title, backgroundImg }) {
+        let style = { backgroundImage: `url(${backgroundImg})`, backgroundColor: '#ececec' }
+        //<img src={require('../images/placeholder250.png')}></img>
+        return (
+            <div className="project" style={style}>
+                <span>{title}</span>
+                {/* <img src={require('../images/placeholder250.png')} /> */}
+            </div>
         );
     }
 
@@ -45,14 +56,9 @@ function Projects({ selectedCat, onChangeFunction, projects }) {
             <div className="projectsWrapper">
                 {projects.map(i => (
                     //Creates a relative link to all the different projects using its ID
-                    <Link className="projectsWrapperChild" key={i.id} to={{
-                        pathname: `/projects/${i.id}`
-                    }}
-                    >
+                    <Link className="projectsWrapperChild" key={i.id} to={{pathname: `/projects/${i.id}`}}>
                         <ProjectThumbnail title={i.title} backgroundImg={i.backgroundURL} />
                     </Link>
-                    
-
                 ))}
             </div>
         </div>
@@ -61,15 +67,3 @@ function Projects({ selectedCat, onChangeFunction, projects }) {
 
 export default Projects;
 
-function ProjectThumbnail({ title, backgroundImg }) {
-    let style = { backgroundImage: `url(${backgroundImg})`, backgroundColor: '#ececec' }
-    //<img src={require('../images/placeholder250.png')}></img>
-    let url = backgroundImg;
-
-    return (
-        <div className="project" style={style}>
-            <span>{title}</span>
-            {/* <img src={require('../images/placeholder250.png')} /> */}
-        </div>
-    );
-}
