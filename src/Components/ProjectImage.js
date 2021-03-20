9c849a5c12e805ac2d1b46437bb55aa4c531ac0d
Andreas <adde.engberg@gmail.com>
2021-03-20 18:50:41 +0100

    Create ProjectImage.js

---
+++ src/Components/ProjectImage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledFigure = styled.figure `
+    display: table;
+    margin: 0 auto;
+`
+
+const Image = styled.img `
+    max-width: 100%;
+    display: block;
+    margin: 0 auto;
+` 
+const Caption = styled.figcaption `
+    display: table-caption;
+    caption-side: bottom;
+    color: gray;
+    font-size: small;
+`
+
+function ProjectImage({source, description}) {
+    
+    return (
+        <StyledFigure>
+        <Image src={source} alt={description} className="projectImage"/> 
+        <Caption>{description}</Caption>
+        </StyledFigure>
+    )
+}
+
+export default ProjectImage
