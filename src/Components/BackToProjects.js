4050e3b13ad0d0684bc7d1c313090f4f8baa7c7a
Andreas <adde.engberg@gmail.com>
2021-03-20 19:02:18 +0100

    Update BackToProjects.js

--- src/Components/BackToProjects.js
+++ src/Components/BackToProjects.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-
-function BackToProjects({_style}) {
+function BackToProjects({style}) {
+    
     return (
         //TODO css button style 
-        <div className="projectBackButton" style={_style}><Link to="/projects"><div>Back to all projects</div></Link></div>
+        <div className="projectBackButton" style={style}><Link to="/projects"><div>Back to all projects</div></Link></div>
     )
 }
 
