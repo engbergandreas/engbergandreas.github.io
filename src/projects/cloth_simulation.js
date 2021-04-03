7f6620668c9dd99450106c3f04b37b9832b5c578
Andreas <adde.engberg@gmail.com>
2021-04-03 10:58:16 +0200

    Added cloth simulation project

--- src/projects/cloth_simulation.js
+++ src/projects/cloth_simulation.js
@@ -3,29 +3,42 @@ import BackButton from '../Components/BackToProjects'
 import ProjectTitle from '../Components/ProjectTitle'
 import ProjectImage from '../Components/ProjectImage'
 import ProjectImages from '../Components/ProjectImages'
-import DemoCodeInfo from '../Components/DemoCodeInfo'
+import DemoCodeLinks from '../Components/DemoCodeInfo'
+import styled from 'styled-components'
 
+const StyledMain = styled.div `
+    padding-top: var(--gap);
+` 
 
 function ClothSimulation() {
     return (
-        <div>
-            <BackButton style={{marginLeft: '20px', justifyContent: 'unset' }}/>
-            <ProjectTitle title ={"THIS IS PROJECT TITLE"} subtitle={"some info about creation date? or other small title"} />
+        <StyledMain>
+            <BackButton top/>
+            <ProjectTitle title ={"CLOTH SIMULATION"} subtitle={"University project created fall 2021"} />
 
             <div className="projectInfo">
                 <h2>SUMMARY</h2>
-                <p>Here is some great info about this project and perhaps a small summary of what the project
-                    is all about. After this perhaps an image or two would be a good idea. I will also need
-                    to come up with some more info so that this paragrahp actually has some length.
+                <p>
+                    The cloth simulation was a project done in the course 'Modeling Course (TNM085)' at Linköpings University during 6½ weeks by a group of 4 people.
+                    The purpose was to simulate a physically based model of their own choosing. 
+                    The model was built using a mass-spring system in a grid like fashion. 
+                    The acting forces are formulated for each mass and Eulers method and Runge Kutta 4 are used to simulate the cloths movement. 
+                    In the project we first implemented a simplified 2D version in Matlab and a Simulink model was developed. 
+                    Later on the system was further implemented in JavaScript using P5.js and WebGL for visualization and texturing, Perlin noise was used to simulate the wind
+                    acting on the cloth.
                 </p>
-                <ProjectImage source={'../images/placeholder1024.png'} description='this image represents bla bla blasad assaddsasdasad  dsads '/>
-                <DemoCodeInfo code="https://github.com/engbergandreas/cloth-simulation" demo="https://engbergandreas.github.io/cloth-simulation/"></DemoCodeInfo>
 
-                <p>then after an image we have some more text to explain something else about the project, which can also include some maths
-                or w/e is nessesary. Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took
-                a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                <ProjectImage source={'../images/cloth/clothsystem.png'} description=''/>
+                <DemoCodeLinks code="https://github.com/engbergandreas/cloth-simulation" demo="https://engbergandreas.github.io/cloth-simulation/"></DemoCodeLinks>
+                <h1 style={{textAlign: 'center'}}>Implementation</h1>
+                
+                <h2>Model</h2>
+                <p>
+                    Our model was built using an m x n grid of particles with a mass connected with springs and dampers. The idea comes from Xavier Provot using 
+                    structural, shear and flexion springs. The structural springs rejects compression and traction, shear springs helps maintain structure and the flexion springs
+                    resists unnatural folding. 
                 </p>
+                <ProjectImage source='../images/cloth/mass-spring-system.png' description='Graphical presentation of the mass-spring system'/>
 
                 <ProjectImages source1={'../images/placeholder250.png'} description1={'this image represents bla bla bla as sadsad ds a dsa sad'}
                     source2={'../images/placeholder640.png'} description2={'this is another descprition hoasdasd'}/>
@@ -42,7 +55,7 @@ function ClothSimulation() {
                 <ProjectImage source='../images/placeholder640.png' description='asdasdd'/>
                 <BackButton />
             </div>
-        </div>
+        </StyledMain>
 
     );
 }
