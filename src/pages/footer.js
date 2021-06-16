5e2cfb7942de4c048a820f6e302c11aeeab51d07
Andreas <adde.engberg@gmail.com>
2021-06-16 23:59:39 +0200

    Added logos to footer

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -11,17 +11,23 @@ const StyledFooter = styled.footer `
     background: #fff;
     display: flex;
     height: 10vh;
-    min-height: 100px;
+    min-height: var(--gap);
     font-size: 1.5em;
+
+    & a {
+        margin: 0 15px;
+    }
 `
 
 
 function Footer() {
     return (
         <StyledFooter>
-            <h2>Linked in</h2>
-            <h2>Github</h2>
-            <h2>Mail</h2>
+            
+            <a href="https://www.linkedin.com/in/andreas-engberg/"><img src="/images/linkedin.png"></img></a>
+            <a href="https://github.com/engbergandreas"><img src="/images/GitHub.png"></img></a>
+            <a href="mailto:adde.engberg@gmail.com"><img src="/images/mail.png"></img></a>
+
 
         </StyledFooter>
     );
