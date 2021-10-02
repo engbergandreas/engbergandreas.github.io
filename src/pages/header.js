a56c1e498eec28d51889be2a33b9324f9a16fb5c
Andreas <adde.engberg@gmail.com>
2021-10-02 11:17:21 +0200

    Update info and css

--- src/pages/header.js
+++ src/pages/header.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components'
 
-const RightHalfHeader = styled.div `
+const RightHalfHeader = styled.div`
 display: flex;
 justify-content: space-evenly;
 width: 375px;
-` 
-const StyledHeader = styled.header `
+`
+const StyledHeader = styled.header`
     position: absolute;
     left: 0;
     right: 0;
-` 
+`
 function Header() {
     return (
         <StyledHeader>
             <nav className="wrapper">
-                <NavLink exact activeClassName="activePage" className="wrapperChild" to="/">Andreas Engberg</NavLink >
+                <NavLink exact activeClassName="activePage" className="wrapperChild" to="/">Home</NavLink >
                 <RightHalfHeader>
                     <NavLink activeClassName="activePage" className="wrapperChild" to="/portfolio">Portfolio</NavLink>
                     <NavLink activeClassName="activePage" className="wrapperChild" to="/projects">Projects</NavLink>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,31 +1,40 @@
 import React from 'react'
 import styled from 'styled-components'
 import Projects from './projects'
+import { NavLink } from 'react-router-dom';
 
-const StyledMain = styled.div `
+
+const StyledMain = styled.div`
     padding-top: var(--gap);
-` 
-const Banner = styled.div `
+`;
+const Banner = styled.div`
     display: flex;
     align-items: center;
     padding: 25px 0 0 100px;
 `;
-const Portrait = styled.img `
+const Portrait = styled.img`
     height: 300px; 
     width: 300px;
     bakground: white;
     border-radius: 50%;
 `;
 
-const IntroName = styled.h1 `
+const IntroName = styled.h1`
     margin: 0;
-    margin-left: 50px;
-`
+    font-size: 2.8em;
+    letter-spacing: -2px;
+    line-height: 55px;
+`;
 
-const Info = styled.div `
+const Info = styled.div`
     width: 50%;
     margin-top: 50px;
-    margin: 50px auto 0 auto;
+    margin-left: 450px;
+`;
+const Wrapper = styled.div`
+    margin: 0;
+    margin-left: 50px;
+    text-align: center;
 `
 
 function home() {
@@ -33,18 +42,26 @@ function home() {
         <StyledMain className="mainContent">
             <Banner>
                 <Portrait src="../images/me.png"></Portrait>
-                <IntroName>Andreas Engberg</IntroName>
+                <Wrapper>
+                    <IntroName>Andreas Engberg</IntroName>
+                    <div>Rolig * Skojig * Kul</div>
+                </Wrapper>
             </Banner>
             <Info>
                 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vulputate consectetur sem,
                     sit amet accumsan sem condimentum eu. Duis vel quam ligula. Quisque ipsum justo, convallis et eros in, fermentum cursus risus.
                     Nunc ullamcorper lobortis viverra. In maximus gravida ante non accumsan. In ac purus sed tortor laoreet aliquet.
                     Nullam gravida lobortis arcu non tincidunt. Phasellus viverra odio ex, sed facilisis nisl vehicula at.
-                    Cras sollicitudin purus at orci iaculis, eget consequat arcu fringilla. 
+                    Cras sollicitudin purus at orci iaculis, eget consequat arcu fringilla.
                     Fusce vehicula orci sed velit mollis, quis egestas nibh sagittis.
                     Mauris metus augue, consectetur eget euismod ac, efficitur sit amet urna.
                     Nulla quis diam sit amet nunc convallis auctor. Curabitur enim nibh,
-                    fermentum congue mi dictum at. </p> 
+                    fermentum congue mi dictum at.
+                </p>
+                <p>
+                    Checkout my projects
+                    <NavLink to="/projects"> here</NavLink>
+                </p>
             </Info>
         </StyledMain>
     );
